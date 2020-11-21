9507c2665fd99b8da8e9c25741759c234e311ff9
Théo Szpera <ickyazer@gmail.com>
2020-11-21 20:54:26 +0100

    add the new cmd in help

--- commands/help.js
+++ commands/help.js
@@ -15,7 +15,7 @@ module.exports.run = async (bot, message, args) => {
         var embed = new Discord.MessageEmbed()
             .setAuthor(`Here is the Avaible Commands to use:`)
             .setDescription('```help  | avatar | cat | csgo | csgofloat | dogs | duckduckgo | f | fart | feed | high-five | img | '+
-            'kiss | kitsune | marry | math | moe | neko | owoify | pat | ping | rps | russianroulette | say | serverinfo | spank | flatearth  | minecraft```')
+            'kiss | kitsune | marry | math | moe | neko | owoify | pat | ping | rps | russianroulette | say | serverinfo | spank | flatearth  | minecraft | mcachievement | meme```')
             .addFields({ name: 'Prefix', value: '```h.```', inline: true})
             .setColor('#00FFF3')
             
